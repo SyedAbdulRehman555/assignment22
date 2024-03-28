5cf45b1df4aba03295e4ab717e2491ee1145c7c5
Syed Abdul Rehman <imabdul555@gmail.com>
2024-03-28 23:19:59 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,68 @@
+// Q1)
+
+// var num = +prompt("enter your number");
+
+// document.write("Number " + num);
+// document.write("Round of Value " + Math.round(num));
+// document.write("floor value " + Math.floor(num));
+// document.write("Ceil Value" + Math.ceil(num));
+
+// Q2)
+
+// var num = +prompt("enter your number");
+
+// document.write("Number " + num);
+// document.write("Round of Value " + Math.round(num));
+// document.write("floor value " + Math.floor(num));
+// document.write("Ceil Value" + Math.ceil(num));
+
+//Q3 )
+
+// var num = -4;
+// var num2 = 0;
+// if (num < 0) {
+//   num2 = num * -1;
+// } else {
+//   num2 = num;
+// }
+// document.write("the absolute value is " + num2);
+
+//Q4)
+
+// var dice = Math.random() * 6;
+
+// var dice2 = Math.ceil(dice);
+
+// console.log(dice2);
+
+// document.write('Random dice value '+dice2)
+
+//Q5)
+
+// var coin = Math.ceil(Math.random() * 2);
+// console.log(coin);
+
+// document.write("Random coin value " + coin);
+
+//Q6)
+
+// var ranNum = Math.ceil(Math.random() * 100);
+
+// document.write("Random Number between 1 and 100 : " + ranNum);
+
+//Q7)
+
+// var userWeight = +prompt("enter your weight");
+
+// document.write(userWeight );
+
+//Q8
+
+// var ranNum = Math.random() * 10;
+// var ranNum2 = Math.ceil(ranNum);
+// var uInput = +prompt("enter your number");
+
+// if (uInput == ranNum2) {
+//   document.write("Congrats you entered the right number");
+// }
+// console.log(ranNum2);
